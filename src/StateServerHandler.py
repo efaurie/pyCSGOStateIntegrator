e613e95c7fd599d104e2511dd3e8e779869cbe16
Eric Faurie <efaurie@hawk.iit.edu>
2015-12-10 11:19:29 -0600

    Intercept any GET or PUT requests and print an error message. Also suppress logging

--- src/StateServerHandler.py
+++ src/StateServerHandler.py
@@ -9,3 +9,15 @@ class StateServerHandler(BaseHTTPRequestHandler):
         print '\nRequest:\n\t' + self.request
         print '\nrFile:\n\t' + self.rfile
 
+    def do_GET(self):
+        self.not_allowed('GET')
+
+    def do_PUT(self):
+        self.not_allowed('PUT')
+
+    def not_allowed(self, message_type):
+        self.send_response(406)
+        print '[-] {0} Received: This server only expects POSTs'.format(message_type)
+
+    def log_message(self, message_format, *args):
+        pass
